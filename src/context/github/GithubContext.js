b12461d07398b6fec512f830bee6bb6fee87b53a
plug47 <oarmoa44@gmail.com>
2022-01-18 15:59:06 -0300

    Use Search Component in process

---
+++ src/context/github/GithubContext.js
@@ -0,0 +1,60 @@
+import { createContext, useReducer } from "react"
+import githubReducer from "./GithubReducer"
+
+const GithubContext = createContext()
+
+const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
+const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
+
+export const GithubProvider = ({children}) => {
+    
+    //NOW, WE USE REDUCERS
+    /*const [users, setUsers] = useState([])
+    const [loading, setLoading] = useState(true)*/
+
+    const initialState = {
+        users: [],
+        loading: false
+    }
+
+    const [state, dispatch] = useReducer(githubReducer, initialState)
+
+    //Get Initial Users (testing purposes)
+    const fetchUsers = async () => {
+        setLoading()
+        const response = await fetch(`${GITHUB_URL}/users`, {
+            headers: {
+                Authorization: `token ${GITHUB_TOKEN}`
+            }
+        })
+
+        const data = await response.json()
+
+        //setUsers(data)
+        //setLoading(false)
+        dispatch({
+            type: 'GET_USERS',
+            payload: data,
+            loading: false
+        })
+    }
+
+    //Set Loading
+    const setLoading = () => {
+        dispatch({
+            type: 'SET_LOADING'
+        })
+    }
+
+    return <GithubContext.Provider value={{
+        //users,            useState
+        users: state.users,
+        //loading,
+        loading: state.loading,
+        fetchUsers
+    }}>
+        {children}
+    </GithubContext.Provider>
+}
+
+export default GithubContext
