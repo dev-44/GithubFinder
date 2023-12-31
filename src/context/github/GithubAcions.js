b182d2d59b24f9d2dc03c3884a60567fe362c228
Oscar Armoa <oarmoa44@gmail.com>
2023-12-31 19:58:15 -0300

    Change of api to get users

--- src/context/github/GithubAcions.js
+++ src/context/github/GithubAcions.js
@@ -32,7 +32,7 @@ export const searchUsers = async (text) => {
 
     //return items
     
-    const response = await github.get(`/search/users?${params}`)
+    const response = await github.get(`/users?${params}`)
     return response.data.items
 
 }
